38ca4698ca75c3d7885652d6aa54685646f22a23
ajzajac <ajzajac@icloud.com>
2020-04-30 15:09:21 -0400

    commented

--- scraper.rb
+++ scraper.rb
@@ -3,18 +3,18 @@ require 'byebug'
 require 'httparty'
 
 def scraper
-    url = 'https://www.indeed.com/jobs?q=software+engineer&l=Brooklyn%2C+NY'
+    url = 'https://www.indeed.com/jobs?q=software+engineer&l=Brooklyn%2C+NY' # change location or job title here for indeed.com
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
     job_list = Array.new
     
-    jobs = parsed_page.css('div.jobsearch-SerpJobCard') # listings per page
-    total = parsed_page.css('div#searchCountPages').text.split(' ')[3].gsub(',','').to_i # number of pages
-    per_page = jobs.count
-    page = 10
-    last_page = ( total.to_f / per_page.to_f ).round
+    jobs = parsed_page.css('div.jobsearch-SerpJobCard') # job listings per page
+    total = parsed_page.css('div#searchCountPages').text.split(' ')[3].gsub(',','').to_i # total number of pages
+    per_page = jobs.count # how many jobs per each page
+    page = 10 # for indeed.com their search pages increment by 10 (10, 20, 30...etc)
+    last_page = ( total.to_f / per_page.to_f ).round # last page to scrape through
 
-    while page <= last_page
+    while page <= last_page # loops through each page of jobs and turns the page
         pagination_url = "https://www.indeed.com/jobs?q=software+engineer&l=Brooklyn%2C+NY&start=#{page}"
         puts "pagination_url"
         puts "Page: #{page}"
@@ -22,7 +22,7 @@ def scraper
         pagination_unparsed_page = HTTParty.get(pagination_url)
         pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
         pagination_jobs = pagination_parsed_page.css('div.jobsearch-SerpJobCard')
-        pagination_jobs.each do |job|
+        pagination_jobs.each do |job| # save each job's info to a hash
         job = {
             title: job.css('h2.title').text,
             company: job.css('span.company').text,
@@ -31,14 +31,15 @@ def scraper
             datePosted: job.css('span.date').text,
         }
         job_list << jobs
-        puts "Added #{job[:title]}"
-        puts "#{job[:location]}"
-        puts "#{job[:datePosted]}"
+        puts "Title: #{job[:title]}"
+        puts "Company: #{job[:company]}"
+        puts "Location: #{job[:location]}"
+        puts "Date Posted: #{job[:datePosted]}"
         puts ''
         puts ''
         end
-        sleep(1)
-        page += 10
+        sleep(1) # trying to delay hitting the page too fast
+        page += 10 # page's on indeed.com increment by 10
         byebug
     end
     
